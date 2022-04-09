45ef47294ebc28f525270d7313e5f477fddf2e04
Danilo Fernandes <dandan158@gmail.com>
2022-04-09 00:27:13 -0300

    init

--- lib/screens/menu_restaurant_screen.dart
+++ lib/screens/menu_restaurant_screen.dart
@@ -5,7 +5,9 @@ import 'package:app_sem_nome/components/score_component.dart';
 import 'package:flutter/material.dart';
 
 class MenuRestaurantScreen extends StatelessWidget {
-  const MenuRestaurantScreen({Key? key}) : super(key: key);
+  final dataKey = new GlobalKey();
+
+  MenuRestaurantScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +18,7 @@ class MenuRestaurantScreen extends StatelessWidget {
           child: Column(
             children: [
               Padding(
-                padding: const EdgeInsets.only(bottom:0),
+                padding: const EdgeInsets.only(bottom: 0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: const [
@@ -34,7 +36,7 @@ class MenuRestaurantScreen extends StatelessWidget {
               Row(
                 children: [
                   const Padding(
-                    padding: EdgeInsets.only( left: 10),
+                    padding: EdgeInsets.only(left: 10),
                     child: Align(alignment: Alignment.topLeft, child: RestaurantLogoSmall()),
                   ),
                   Padding(
@@ -74,25 +76,61 @@ class MenuRestaurantScreen extends StatelessWidget {
                                   'Ver mais Informacoes',
                                   style: TextStyle(fontSize: 12),
                                 ),
-                                Icon(Icons.arrow_forward_ios, size: 12,)
+                                Icon(
+                                  Icons.arrow_forward_ios,
+                                  size: 12,
+                                )
                               ],
                             ),
                           ),
                         ),
                         const Padding(
                           padding: EdgeInsets.only(top: 30),
-                          child: Text('Mesa 10', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                          child: Text(
+                            'Mesa 10',
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
                         ),
                       ],
                     ),
                   )
                 ],
               ),
-                        const Divider(color: Colors.black,)
+              const Divider(
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  padding: const EdgeInsets.all(5),
+                  children: _generateCategoryTab(),
+                ),
+              ),
             ],
           ),
         ),
       ),
     );
   }
+}
+
+List<Widget> _generateCategoryTab() {
+  List<Widget> lista = [];
+  List<String> mocksNames = ["Combo", "Promoção", "Carnes", "Peixe", "Burguer"];
+  for (String name in mocksNames) {
+    lista.add(Padding(
+      padding: const EdgeInsets.only(left: 5, right: 5),
+      child: ElevatedButton(
+        onPressed: () => print('clicou em aba'),
+        child: Text(name),
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.white),
+          foregroundColor: MaterialStateProperty.all(Colors.black),
+        ),
+      ),
+    ));
+  }
+
+  return lista;
 }
